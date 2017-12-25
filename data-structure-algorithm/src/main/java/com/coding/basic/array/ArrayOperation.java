658143bc7bf35a668bc3d21d20ea37a8d0839b38
ByrsH <bgxjsh@sina.com>
2017-12-25 21:59:54 +0800

    fix list operation

--- data-structure-algorithm/src/main/java/com/coding/basic/array/ArrayOperation.java
+++ data-structure-algorithm/src/main/java/com/coding/basic/array/ArrayOperation.java
@@ -14,15 +14,15 @@ public class ArrayOperation {
 
     public static void sameAndDifferentOperation(List<Integer> a, List<Integer> b, List<Integer> sameItems,
                                                  List<Integer> onlyAItems, List<Integer> onlyBItems){
-        if (isEmpty(a) && isEmpty(b)){
+        if (a.isEmpty() && b.isEmpty()){
             return;
         }
-        if (isEmpty(a)){
+        if (a.isEmpty()){
             HashSet<Integer> hs = new HashSet<>(b);
             onlyBItems.clear();
             onlyBItems.addAll(hs);
         }
-        if (isEmpty(b)){
+        if (b.isEmpty()){
             HashSet<Integer> hs = new HashSet<>(a);
             onlyAItems.clear();
             onlyAItems.addAll(hs);
@@ -56,31 +56,49 @@ public class ArrayOperation {
 
     public static void sameAndDifferentOperation2(List<CarProvinceMarket> a, List<CarProvinceMarket> b, List<CarProvinceMarket> sameItems,
                                                  List<CarProvinceMarket> onlyAItems, List<CarProvinceMarket> onlyBItems){
+
+        if (a.isEmpty() && b.isEmpty()){
+            return;
+        }
+        if (a.isEmpty()){
+            HashSet<CarProvinceMarket> hs = new HashSet<>(b);
+            onlyBItems.clear();
+            onlyBItems.addAll(hs);
+        }
+        if (b.isEmpty()){
+            HashSet<CarProvinceMarket> hs = new HashSet<>(a);
+            onlyAItems.clear();
+            onlyAItems.addAll(hs);
+        }
+
         HashMap<Integer, CarProvinceMarket> map = new HashMap<>();
+        HashMap<Integer, CarProvinceMarket> sameMap = new HashMap<>();
+        HashMap<Integer, CarProvinceMarket> onlyBMap = new HashMap<>();
         for (CarProvinceMarket item: a){
             map.put(item.getProvinceId(), item);
         }
 
         for (CarProvinceMarket item: b){
             if (map.containsKey(item.getProvinceId())){
-                sameItems.add(item);
+                sameMap.put(item.getProvinceId(), item);
             }else {
-                onlyBItems.add(item);
+                onlyBMap.put(item.getProvinceId(),item);
             }
         }
 
+        sameItems.addAll(sameMap.values());
+        onlyBItems.addAll(onlyBMap.values());
 
-    }
-
-    public static Boolean isEmpty(List<Integer> a){
-        if (a == null || a.size() == 0){
-            return true;
-        }else {
-            return false;
+        for (CarProvinceMarket item: sameItems){
+            map.remove(item.getProvinceId());
         }
+
+        onlyAItems.addAll(map.values());
+
     }
 
     public static void main(String [] args){
+        /*
         ArrayList<Integer> a = new ArrayList<>();
         ArrayList<Integer> b = new ArrayList<>();
         for (int i=0; i<1000000; i++){
@@ -96,12 +114,27 @@ public class ArrayOperation {
 
         System.out.println("time = " + (System.currentTimeMillis() - start));
 
+        */
         System.out.println("======================");
 
-        HashMap<Integer, Integer> set = new HashMap<>();
-        set.put(1000000,1);
-        set.put(1000000,2);
-        System.out.println(set.size());
+        ArrayList<CarProvinceMarket> aCarProMar = new ArrayList<>();
+        ArrayList<CarProvinceMarket> bCarProMar = new ArrayList<>();
+        for (int i=0; i<1000000; i++){
+            CarProvinceMarket aCar = new CarProvinceMarket();
+            aCar.setProvinceId((i + 1)/2);
+            aCarProMar.add(aCar);
+            CarProvinceMarket bCar = new CarProvinceMarket();
+            bCar.setProvinceId((i + 2)/2);
+            bCarProMar.add(bCar);
+        }
+
+        LinkedList<CarProvinceMarket> sameItems = new LinkedList<>();
+        LinkedList<CarProvinceMarket> onlyAItems = new LinkedList<>();
+        LinkedList<CarProvinceMarket> onlyBItems = new LinkedList<>();
+
+        Long start = System.currentTimeMillis();
+        sameAndDifferentOperation2(aCarProMar, bCarProMar, sameItems, onlyAItems, onlyBItems);
+        System.out.println("time = " + (System.currentTimeMillis() - start));
 
     }
 }
