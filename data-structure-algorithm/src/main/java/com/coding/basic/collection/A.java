39800aa4845025ed50c500bb85f8c61faec6c0d2
ByrsH <bgxjsh@sina.com>
2018-04-07 22:00:17 +0800

    fix

---
+++ data-structure-algorithm/src/main/java/com/coding/basic/collection/A.java
@@ -0,0 +1,9 @@
+package com.coding.basic.collection;
+
+/**
+ * @author ByrsH
+ * @create 2018-03-04 12:38
+ **/
+
+public class A {
+}
---
+++ data-structure-algorithm/src/main/java/com/coding/basic/collection/map/SortedSetDemo.java
@@ -0,0 +1,37 @@
+package com.coding.basic.collection.map;
+
+import net.mindview.util.CountingMapData;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+import net.mindview.util.*;
+import static net.mindview.util.Print.*;
+
+/**
+ * treeMap测试demo
+ *
+ * @author ByrsH
+ * @create 2018-03-04 12:40
+ **/
+
+public class SortedSetDemo {
+    public static void main(String [] args){
+        TreeMap<Integer, String> sortedMap = new TreeMap<Integer, String>(new CountingMapData(10));
+        print(sortedMap);
+        Integer low = sortedMap.firstKey();
+        Integer high = sortedMap.lastKey();
+        print(low);
+        print(high);
+        Iterator<Integer> it = sortedMap.keySet().iterator();
+        for (int i=0; i<=6; i++){
+            if (i == 3) low = it.next();
+            if (i == 6) high = it.next();
+            else it.next();
+        }
+        print(low);
+        print(high);
+        print(sortedMap.subMap(low, high));
+        print(sortedMap.headMap(high));
+        print(sortedMap.tailMap(low));
+    }
+}
